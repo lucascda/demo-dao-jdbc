83a2aa9e815177ad3c13b162c6686c41ccf09510
Lucas César <lucascesar_35@hotmail.com>
2021-05-01 02:03:00 -0300

    add: dao model & dao factory

---
+++ src/main/java/model/dao/DaoFactory.java
@@ -0,0 +1,11 @@
+package model.dao;
+
+import db.Db;
+import model.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+    
+    public static SellerDao createSellerDao(){
+        return new SellerDaoJDBC(Db.getConnection());
+    }  
+}
---
+++ src/main/java/model/impl/SellerDaoJDBC.java
@@ -0,0 +1,50 @@
+package model.impl;
+
+import java.sql.Connection;
+import java.util.List;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao{
+
+    private Connection conn;
+    
+
+    public SellerDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
+    
+
+    @Override
+    public void insert(Seller obj) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void update(Seller obj) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
