a1a4f40344f7654e794d0668774df77dfa9fb7d1
Lucas César <lucascesar_35@hotmail.com>
2021-05-04 22:48:51 -0300

    add: DaoFactory changes

--- src/main/java/model/dao/DaoFactory.java
+++ src/main/java/model/dao/DaoFactory.java
@@ -1,11 +1,16 @@
 package model.dao;
 
 import db.Db;
+import model.impl.DepartmentDaoJDBC;
 import model.impl.SellerDaoJDBC;
 
 public class DaoFactory {
     
     public static SellerDao createSellerDao(){
         return new SellerDaoJDBC(Db.getConnection());
-    }  
+    }
+    
+    public static DepartmentDao createDepartmentDao(){
+        return new DepartmentDaoJDBC(Db.getConnection());
+    }
 }
